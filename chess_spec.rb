508a8baa22482c602f573528013366e4294490a5
Lauren N. Roth & Max Rater & Kurt Schnelker & Tracy Teague <pair+laurennicoleroth.maxrater.sabron13.tracyteague@devbootcamp.com>
2015-03-06 15:20:43 -0500

    Rspec file back in business

--- chess_spec.rb
+++ chess_spec.rb
@@ -25,116 +25,116 @@ describe 'Board' do
   end
 end
 
-# describe 'Rook' do
-#   let (:rook) { Rook.new({7 => 7}, "black") }
+describe 'Rook' do
+  let (:rook) { Rook.new({7 => 7}, "black") }
 
-#   it 'starts position and color' do
-#     expect(rook.color).to eq("black")
-#   end
+  it 'starts position and color' do
+    expect(rook.color).to eq("black")
+  end
 
-#   it 'has valid? method which returns true' do
-#     expect(rook.valid?({ 6=>7 })).to eq(true)
-#   end
+  it 'has valid? method which returns true' do
+    expect(rook.valid?({ 6=>7 })).to eq(true)
+  end
 
-#   it 'has valid? method which returns false' do
-#     expect(rook.valid?({ 6=>6 })).to eq(false)
-#   end
-# end
+  it 'has valid? method which returns false' do
+    expect(rook.valid?({ 6=>6 })).to eq(false)
+  end
+end
 
-# describe 'Knight' do
-#   let (:knight) { Knight.new({1 => 0}, "white") }
+describe 'Knight' do
+  let (:knight) { Knight.new({1 => 0}, "white") }
 
-#   it 'starts position and color' do
-#     expect(knight.color).to eq("white")
-#   end
+  it 'starts position and color' do
+    expect(knight.color).to eq("white")
+  end
 
-#   it 'has valid? method which returns true' do
-#     expect(knight.valid?({ 2 => 2 })).to eq(true)
-#   end
+  it 'has valid? method which returns true' do
+    expect(knight.valid?({ 2 => 2 })).to eq(true)
+  end
 
-#   it 'has valid? method which returns false' do
-#     expect(knight.valid?({ 2 => 3 })).to eq(false)
-#   end
-# end
+  it 'has valid? method which returns false' do
+    expect(knight.valid?({ 2 => 3 })).to eq(false)
+  end
+end
 
-# describe 'Bishop' do
-#   let (:bishop) { Bishop.new({5 => 7}, "black") }
+describe 'Bishop' do
+  let (:bishop) { Bishop.new({5 => 7}, "black") }
 
-#   it 'starts position and color' do
-#     expect(bishop.color).to eq("black")
-#   end
+  it 'starts position and color' do
+    expect(bishop.color).to eq("black")
+  end
 
-#   it 'has valid? method which returns true' do
-#     expect(bishop.valid?({ 6=>6 })).to eq(true)
-#   end
+  it 'has valid? method which returns true' do
+    expect(bishop.valid?({ 6=>6 })).to eq(true)
+  end
 
-#   it 'has valid? method which returns false' do
-#     expect(bishop.valid?({ 6=>5 })).to eq(false)
-#   end
-# end
+  it 'has valid? method which returns false' do
+    expect(bishop.valid?({ 6=>5 })).to eq(false)
+  end
+end
 
-# describe 'King' do
-#   let (:king) { King.new({4 => 0}, "white") }
+describe 'King' do
+  let (:king) { King.new({4 => 0}, "white") }
 
-#   it 'starts position and color' do
-#     expect(king.color).to eq("white")
-#   end
+  it 'starts position and color' do
+    expect(king.color).to eq("white")
+  end
 
-#   it 'has valid? method which returns true' do
-#     expect(king.valid?({ 4=>1 })).to eq(true)
-#   end
+  it 'has valid? method which returns true' do
+    expect(king.valid?({ 4=>1 })).to eq(true)
+  end
 
-#   it 'has valid? method which returns false' do
-#     expect(king.valid?({ 4=>2 })).to eq(false)
-#   end
-# end
+  it 'has valid? method which returns false' do
+    expect(king.valid?({ 4=>2 })).to eq(false)
+  end
+end
 
-# describe 'Queen' do
-#   let (:queen) { Queen.new({3 => 0}, "white") }
+describe 'Queen' do
+  let (:queen) { Queen.new({3 => 0}, "white") }
 
-#   it 'starts position and color' do
-#     expect(queen.color).to eq("white")
-#   end
+  it 'starts position and color' do
+    expect(queen.color).to eq("white")
+  end
 
-#   it 'has valid? method which returns true' do
-#     expect(queen.valid?({ 3=>5 })).to eq(true)
-#   end
+  it 'has valid? method which returns true' do
+    expect(queen.valid?({ 3=>5 })).to eq(true)
+  end
 
-#   it 'has valid? method which returns false' do
-#     expect(queen.valid?({ 4=>2 })).to eq(false)
-#   end
-# end
+  it 'has valid? method which returns false' do
+    expect(queen.valid?({ 4=>2 })).to eq(false)
+  end
+end
 
-# describe 'Pawn' do
-#   let (:pawn) { Pawn.new({4 => 6}, "black") }
+describe 'Pawn' do
+  let (:pawn) { Pawn.new({4 => 6}, "black") }
 
-#   it 'starts position and color' do
-#     expect(pawn.color).to eq("black")
-#   end
+  it 'starts position and color' do
+    expect(pawn.color).to eq("black")
+  end
 
-#   it 'has valid? method which returns true' do
-#     expect(pawn.valid?({ 4 => 5 })).to eq(true)
-#   end
+  it 'has valid? method which returns true' do
+    expect(pawn.valid?({ 4 => 5 })).to eq(true)
+  end
 
-#   it 'has valid? method which returns false' do
-#     expect(pawn.valid?({ 5 => 6 })).to eq(false)
-#   end
-# end
+  it 'has valid? method which returns false' do
+    expect(pawn.valid?({ 5 => 6 })).to eq(false)
+  end
+end
 
-# describe 'Pawn' do
-#   let (:pawn) { Pawn.new({7 => 1}, "white") }
+describe 'Pawn' do
+  let (:pawn) { Pawn.new({7 => 1}, "white") }
 
-#   it 'starts position and color' do
-#     expect(pawn.color).to eq("white")
-#   end
+  it 'starts position and color' do
+    expect(pawn.color).to eq("white")
+  end
 
-#   it 'has valid? method which returns true' do
-#     expect(pawn.valid?({ 7 => 2 })).to eq(true)
-#   end
+  it 'has valid? method which returns true' do
+    expect(pawn.valid?({ 7 => 2 })).to eq(true)
+  end
 
-#   it 'has valid? method which returns false' do
-#     expect(pawn.valid?({ 6 => 5 })).to eq(false)
-#   end
-# end
+  it 'has valid? method which returns false' do
+    expect(pawn.valid?({ 6 => 5 })).to eq(false)
+  end
+end
 
 
