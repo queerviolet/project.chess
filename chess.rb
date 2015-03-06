0844694dd0bfb6a8d087b8309024c888cc5ab4c5
Dan Asselin & Hoa Nguyen & Awesome Ninja Fighter & suprfrye <pair+danasselin.hoathenguyen85.samguergen.suprfrye@devbootcamp.com>
2015-03-06 17:32:45 -0500

    Add Game class skeleton

--- chess.rb
+++ chess.rb
@@ -59,3 +59,61 @@ class King < Piece
 end
 # test_piece = Piece.new([0,0], "black", [1,0])
 # p test_piece.move(1,0)
+
+
+# ----------------------------------------------------------------------------------
+
+
+class Game
+  attr_reader :board
+  #creates new board object in initialize
+  def initialize
+    #needs a data structure to hold player_1 and player_2
+    #@board = Board.new
+  end
+  #prints board after every move
+  #to_s expects the board object to have a #board attr_reader
+  def to_s
+    @board.board.map do |row|
+      "#{row}"
+    end.join("\n")
+  end
+  #strips user input [col, row] into row, col format
+  #expects a move array with 2 elements
+  def strip_move(move)
+    alpha = [*"a".."z"]
+    alpha.map.with_index do |letter, i|
+      if move[0] == letter
+        move[0] = i
+      end
+    end
+      move.reverse
+  end
+
+  #alternates player turns UNTIL game OVER
+  def play
+  #UNTIL check_mate?
+  #player_1.turn(gets.chomp)
+  #player_2.turn(gets.chomp)
+  #ENDUNTIL
+  end
+
+  #checks to see if there is a check
+  def check(player)
+
+  end
+
+  def check_mate(player)
+
+  end
+
+  def turn(move)
+    # determine piece-type
+    # ask piece if move is valid for piece-type
+    # ask board if cell is empty
+    # if board is empty AND move is valid, move piece
+    # if cell filled by piece, ask if piece is capturable
+    # if YES, capture and move
+    # if NO, return "invalid move"
+  end
+end
