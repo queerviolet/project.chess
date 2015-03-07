3b5b711f1e77a0b45145a2f43a1356f0e2cba026
suprfrye <tara.c.frye@gmail.com>
2015-03-07 17:44:10 -0500

    Update Piece#valid_move? to be a ratio, remove logic from Piece#move so that it only updates Piece location

--- chess.rb
+++ chess.rb
@@ -1,8 +1,8 @@
 class Piece
   attr_reader :state
-  def initialize(location, state, move_pattern, board, args={}) # pass board in as argument
+  def initialize(location, state, move_pattern, args={}) # pass board in as argument
     # use current @board so that pieces are aware of board methods
-    @board = board
+    # @board = board
     @location = location
     @state = state
     @move_pattern = move_pattern
@@ -12,21 +12,11 @@ class Piece
   end
 
   def valid_move?(row,col)
-    @move_pattern == [@location[0] - row, @location[1] - col].map { |el| el.abs}
+    @move_pattern[0]/@move_pattern[1] == [(@location[0] - row)/ (@location[1] - col)].abs
   end
 # board move may return to/call update_board, place_on_cell, and/or remove
   def move(row,col)
-    if valid_move?(row,col)
-      if @board.cell_empty?(row,col)
-        @location = [row,col]
-        #@board.update_board
-        #@board.place_on_cell(row,col)
-      elsif victim_capturable?(row,col)
-        @location = [row,col]
-        #@board.remove(row,col)
-        #@board.update_board
-      end
-    end
+    @location = [row,col]
   end
 
   def victim_capturable?(victim_state)
@@ -35,6 +25,7 @@ class Piece
 end
 
 class Pawn < Piece
+
 
 end
 
@@ -57,99 +48,104 @@ end
 class King < Piece
 
 end
-# test_piece = Piece.new([0,0], "black", [1,0])
+♟ = Pawn.new([0,0], "black", [1,0], can_reverse: false, capture_pattern: [1,1])
+p ♟.is_a?(Pawn)
+
+# puts "Testing chess symbols"
+# ♖ = "rook"
+# p ♖
 # p test_piece.move(1,0)
 
 
 # ----------------------------------------------------------------------------------
 
 
-class Game
-  attr_reader :board
-  #creates new board object in initialize
-  def initialize
-    #needs a data structure to hold player_1 and player_2
-    #@board = Board.new
-  end
-  #prints board after every move
-  #to_s expects the board object to have a #board attr_reader
-  def to_s
-    @board.board.map do |row|
-      "#{row}"
-    end.join("\n")
-  end
-  #strips user input [col, row] into row, col format
-  #expects a move array with 2 elements
-  def strip_move(move)
-    alpha = [*"a".."z"]
-    alpha.map.with_index do |letter, i|
-      if move[0] == letter
-        move[0] = i
-      end
-    end
-      move.reverse
-  end
+# class Game
+#   attr_reader :board
+#   #creates new board object in initialize
+#   def initialize
+#     #needs a data structure to hold player_1 and player_2
+#     #@board = Board.new
+#   end
+#   #prints board after every move
+#   #to_s expects the board object to have a #board attr_reader
+#   def to_s
+#     @board.board.map do |row|
+#       "#{row}"
+#     end.join("\n")
+#   end
+#   #strips user input [col, row] into row, col format
+#   #expects a move array with 2 elements
+#   def strip_move(move)
+#     alpha = [*"a".."z"]
+#     alpha.map.with_index do |letter, i|
+#       if move[0] == letter
+#         move[0] = i
+#       end
+#     end
+#       move.reverse
+#   end
 
-  #alternates player turns UNTIL game OVER
-  def play
-  #UNTIL check_mate?
-  #player_1.turn(gets.chomp)
-  #player_2.turn(gets.chomp)
-  #ENDUNTIL
-  end
+#   #alternates player turns UNTIL game OVER
+#   def play
+#   #UNTIL check_mate?
+#   #player_1.turn(gets.chomp)
+#   #player_2.turn(gets.chomp)
+#   #ENDUNTIL
+#   end
 
-  #checks to see if there is a check
-  def check(player)
+#   #checks to see if there is a check
+#   def check(player)
 
-  end
+#   end
 
-  def check_mate(player)
+#   def check_mate(player)
 
-  end
+#   end
 
-  def turn(move)
-    # determine piece-type
-    # ask piece if move is valid for piece-type
-    # ask board if cell is empty
-    # if board is empty AND move is valid, move piece
-    # if cell filled by piece, ask if piece is capturable
-    # if YES, capture and move
-    # if NO, return "invalid move"
-  end
-end
-
-
-# ----------------------------------------------------------------------------------
+#   def turn(move)
+#     # determine piece-type
+#     # ask piece if move is valid for piece-type
+#     # ask board if cell is empty
+#     # if board is empty AND move is valid, move piece
+#     # if cell filled by piece, ask if piece is capturable
+#     # if YES, capture and move
+#     # if NO, return "invalid move"
+#   end
+# end
 
 
-class Board
-attr_accessor :board, :captured_pieces
-  def initialize
-    @board = [["♜""♞""♝""♛""♚""♝""♞""♜"],
-    ["♟"*8],
-    [" "*8],
-    [" "*8],
-    [" "*8],
-    [" "*8],
-    ["♙"*8],
-    ["♖""♘""♗""♕""♔""♗""♘""♖"]].split("")
-    @captured_pieces = []
-  end
+# # ----------------------------------------------------------------------------------
 
-  def cell_empty?(y,x)
-    @board[y][x] == " "
-  end
 
-  def remove(y,x)
-    @board[y][x] = " "
-  end
+# class Board
+# attr_accessor :board, :captured_pieces
+#   def initialize
+#     @board = [["♜""♞""♝""♛""♚""♝""♞""♜"],
+#     ["♟"*8],
+#     [" "*8],
+#     [" "*8],
+#     [" "*8],
+#     [" "*8],
+#     ["♙"*8],
+#     ["♖""♘""♗""♕""♔""♗""♘""♖"]].split("")
+#     @captured_pieces = []
+#   end
 
-  def send_piece(y,x)
-    @captured_pieces << @board[y][x]
-  end
+#   def cell_empty?(y,x)
+#     @board[y][x] == " "
+#   end
 
-  def update_board
-    #scans every piece's new location and updates on board
-  end
+#   def remove(y,x)
+#     @board[y][x] = " "
+#   end
 
-end
+#   def send_piece(y,x)
+#     @captured_pieces << @board[y][x]
+#   end
+
+#   def update_board
+#     #scans every piece's new location and updates on board
+#   end
+
+# end
