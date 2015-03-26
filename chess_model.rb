b2331338f927047b6db90cc8bcb87c7c9e56cfc7
Debbie Milburn <deborah.a.milburn@gmail.com>
2015-03-26 11:11:39 -0400

    Initial commit

---
+++ chess_model.rb
@@ -0,0 +1,53 @@
+class Pawn
+
+  def initialize
+  end
+
+end
+
+class Knight
+
+  def initialize
+  end
+
+
+end
+
+class Rook
+
+  def initialize
+  end
+
+end
+
+class Bishop
+
+  def initialize
+  end
+
+
+end
+
+class Queen
+
+  def initialize
+  end
+
+end
+
+class King
+
+  def initialize
+  end
+
+end
+
+
+
+
+class Board
+
+  def initialize
+  end
+
+end
---
+++ chess_model_spec.rb
@@ -0,0 +1,27 @@
+require_relative "chess_model"
+
+
+
+describe "Board" do
+
+end
+
+describe "Pawn" do
+
+end
+
+describe "Knight" do
+
+end
+
+describe "Bishop" do
+
+end
+
+describe "Queen" do
+
+end
+
+describe "King" do
+
+end
