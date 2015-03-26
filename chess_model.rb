69eec32e073ecd75ed716bc3fbbefc19f35923bf
Debbie Milburn <deborah.a.milburn@gmail.com>
2015-03-26 15:58:52 -0400

    Pseudocode

--- chess_model.rb
+++ chess_model.rb
@@ -1,8 +1,29 @@
+#later:
+#castling
+#stalemate
+#check/check mate
+#promotion
+#undo_move
+#speed chess
+#piece class?
+
+north = [1,0]
+south = [-1, 0]
+west = [0, -1]
+east = [0, 1]
+northeast = [1, 1]
+northwest = [1, -1]
+southeast = [-1. 1]
+southwest = [-1, -1]
+
+
 class Pawn
-  #input
-  #output
+
 
   def initialize
+    #argument hash
+    #arguments: position, color, how_it_moves
+    #first_move starts true
   end
 
 end
@@ -72,27 +93,45 @@ class Board
   end
 
   def get_object_from_position
-
+    #input: position in array
+    #output: the object in that position
   end
 
   def check_move_helper
-    #input:
+    #input: object
+    #output: object, move method direction
   end
 
   def rqb_move
     #rook,queen,bishop
-    #input: position
+    #input: object
     #output: array of valid moves
-    #base case:
-    #if space in path has no object, push to valid move array
+    #recursively check in all directions that correspond to the piece
+    #if space in path has no object, push to valid move array and then check again in that direction
+    #false if the space != and piece is the same as the object it hits change directions
+    #false if the space != nil and piece is the opposite of color push to array and change directions
+    #false if the space is not in the array
   end
 
-  def king_knight_move
+  def kk_move
+    #input: object
+    #output: returns array of valid moves
+    #checks all 8 possibilities
 
   end
 
   def pawn_move
+    #input: object
+    #output: array of valid moves
+    # if first_move is true, then check for two spaces as well set first_move to false
+    #evaluate diagonals for enemy pieces
+    #evaluate one space
+  end
 
+  def move
+    #input: take piece
+    #output: return the changed board
+    #modifies the board_array and piece.position
   end
 
 end
