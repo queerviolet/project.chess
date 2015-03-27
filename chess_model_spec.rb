bfa206ee107a7051b3b51b56db411cd7f5538256
costolo <ben.costolo@gmail.com>
2015-03-27 10:50:17 -0400

    add additional test

--- chess_model_spec.rb
+++ chess_model_spec.rb
@@ -1,10 +1,10 @@
 require_relative "chess_model"
 
 describe "Board" do
-	let(:board) {Board.new}
+	let(:chess_board) {Board.new}
 	describe "initialize" do
 		it "should initialize to nil" do
-			board.board.each do |row|
+			chess_board.board.each do |row|
 				row.each do |space|
 					expect(space).to eq nil
 				end
@@ -14,8 +14,29 @@ describe "Board" do
 
 	describe "initialize_white_pieces" do
 		it "should create an array of white piece objects" do
-
+			chess_board.white_pieces_array do |piece|
+				expect(piece.color).to eq "white"
+			end
 		end
+	end
+
+	describe "initialize_black_pieces" do
+		it "should create an array of black piece objects" do
+			chess_board.black_pieces_array do |piece|
+				expect(piece.color).to eq "black"
+			end
+		end
+	end
+
+	describe "place" do
+		it "should push a piece object into the board array corresponding to the piece's position" do
+			args = {color: "white", position: [1,0]}
+			chess_board.place(Pawn.new(args), [5,0])
+			expect(chess_board.board[5][0].is_a?(Pawn)).to eq true
+		end
+	end
+
+	describe "place" do
 	end
 end
 
