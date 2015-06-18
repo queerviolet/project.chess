16316266b7de0c6814aa70584fe81eb56d5fcb6c
Ashi Krishnan <me@ashi.works>
2015-06-18 18:34:01 -0400

    rspec cleanup

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,10 +1,12 @@
+require_relative '../board'
+
 describe 'a board' do
-  let (:board) { Chessboard.new }
+  let (:board) { Chess::Board.new }
 
   it 'starts with 32 pieces, 16 black, 16 white' do
     expect(board.pieces.length).to equal(32)
-    expect(board.pieces.count { |piece| piece.color == :white }).to equal(16)
-    expect(board.pieces.count { |piece| piece.color == :black }).to equal(16)
+    expect(board.pieces.count { |k, w| w.color == :white }).to equal(16)
+    expect(board.pieces.count { |k, b| b.color == :black }).to equal(16)
   end
 
   it 'has a width and height of 8' do
@@ -12,24 +14,7 @@ describe 'a board' do
     expect(board.height).to equal(8)
   end
 
-  it 'has a clear method which removes all pieces' do
-    board.clear
-    expect(board.pieces.empty?).to equal(true)
-  end
-
-  # TODO: We are assuming that Board.new initializes the chess board
-  # correctly. Write a spec for that.
-
-  describe 'has a moves method' do
-    it 'which returns an empty array when called on an empty square' do
-      board.clear
-      expect(board.moves(:white, [0, 0])).to equal([])
-    end
-
-    it 'which returns an empty array when called on a piece of the wrong color' do
-      # row 1, col 0 should be a white pawn
-      expect(board.moves(:black, [1, 0])).to equal([])
-    end
-
+  it 'returns squares when indexed with []' do
+    expect(board[0, 0]).to be_a Chess::Square
   end
 end
